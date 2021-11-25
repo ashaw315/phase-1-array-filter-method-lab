5af411c96a7cbb8891a14573efdcd14cda3c5bf4
Adam <ashaw315@gmail.com>
2021-11-25 13:36:01 -0500

    commit tree 55787f92bb360361d7e66b27cb0c810d85e6704e

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution here
+
+
+function findMatching(driver, name) {
+    return driver.filter(function(person) {
+        return person.toLowerCase() .indexOf(name.toLowerCase()) !==-1
+    })
+}
+
+findMatching(drivers, "Sammy") 
+
+
+function findMatching(drivers, name) {
+    const singleDriver = drivers.filter(person => person.toLowerCase() === name.toLowerCase())
+        return (singleDriver);
+    }
+
+findMatching(drivers, "bobby");
+
+function fuzzyMatch(drivers, name) {
+    const firstLetter = drivers.filter((person) => person.startsWith(name));
+    return firstLetter;
+}
+
+function matchName(drivers, string) {
+    const match = drivers.filter(driver => driver.name === string)
+    return match;
+    
+}
+
+matchName(drivers, "Bobby");
+
+
+
+
+
+
+
+
+
+
+
+
+
